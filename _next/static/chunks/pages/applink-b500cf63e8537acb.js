9319c5851770cac38ca060fb732326666be4abe5
FPabl0 <f.pablo1@hotmail.com>
2025-02-09 17:38:02 -0500

    Updates

---
+++ _next/static/chunks/pages/applink-b500cf63e8537acb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[279],{9520:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/applink",function(){return t(800)}])},800:function(n,u,t){"use strict";t.r(u);var _=t(5893);let e=()=>(0,_.jsx)(_.Fragment,{});u.default=e}},function(n){n.O(0,[774,888,179],function(){return n(n.s=9520)}),_N_E=n.O()}]);
